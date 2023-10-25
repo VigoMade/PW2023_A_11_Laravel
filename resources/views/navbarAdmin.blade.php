122acb1d3c44083661542c19526eb228a086657c
VigoMade <vigovigo129@gmail.com>
2023-10-25 15:00:13 +0700

    fix lagi

--- resources/views/navbarAdmin.blade.php
+++ resources/views/navbarAdmin.blade.php
@@ -8,6 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <style>
+        .bg-image {
+            width: 100vw;
+            height: 100hv;
+            object-fit: cover;
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            filter: brightness(0.6);
+        }
+
         .navbar.navbar-scrolled {
             background-color: #03213B;
         }
@@ -19,6 +30,7 @@
             box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
 
         }
+
 
         .navbar.navbar-scrolled .dropdown-item {
             color: white;
@@ -32,86 +44,49 @@
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-dark fixed-top" id="custom-navbar">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
+
+            <div class="btn-group">
+                <button id="dropdown-button" type="button" class="btn" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false" style="background-color: #03213B; color: white;">
+                    <img src="{{asset('img/avatar2.png')}}" alt="Avatar" class="rounded-circle" style="width: 32px; height: 32px;"> Admin <i class="fas fa-caret-down"></i>
+                </button>
+
+                <ul class=" dropdown-menu dropdown-menu-lg-end" style="background-color: #03213B;">
+                    <li><a class=" dropdown-item" href="{{url('loginAdmin')}}" style="color: white;" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                <nav class="navbar navbar-dark fixed-top" id="custom-navbar">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="btn-group">
-                            <button id="dropdown-button" type="button" class="btn" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false" style="background-color: #03213B; color: white;">
-                                <img src="{{asset('img/avatar2.png')}}" alt="Avatar" class="rounded-circle" style="width: 32px; height: 32px;"> Admin <i class="fas fa-caret-down"></i>
-                            </button>
-
-                            <ul class=" dropdown-menu dropdown-menu-lg-end" style="background-color: #03213B;">
-                                <li><a class=" dropdown-item" href="{{url('loginAdmin')}}" style="color: white;" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
-                <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel" style="background-color:#03213B !important;">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Admin</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="{{url('admin')}}">Admin Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{url('daftar')}}">Daftar Film</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{url('userControl')}}">User Control</a>
-                            </li>
-                        </ul>
-                        <form class="d-flex mt-3" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
             </div>
+        </div>
     </nav>
 
+    <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel" style="background-color:#03213B !important;">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Admin</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{url('admin')}}">Admin Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('daftar')}}">Daftar Film</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('userControl')}}">User Control</a>
+                </li>
+            </ul>
+            <form class="d-flex mt-3" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+    </div>
+    </nav>
 
     <div class="content-wrapper">
         @yield('content')
